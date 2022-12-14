d6684ab9b2e060563d095c18dbc38b57ec28ba08
Enrique Saracho Felix <esarachof97@gmail.com>
2022-12-13 19:01:41 -0700

    index.html app.js styles.css
    Updated files, worked on navbar, added transitions and media queries

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const hamburgerButton = document.getElementById("hamburger");
-const navList = document.getElementById("nav-list");
+const navBar = document.getElementById("nav-bar");
+const body = document.getElementById("body");
+let flag = 0;
 
 function toggleButton() {
-  navList.classList.toggle("show");
+  if(flag == 0) {
+    navBar.style.left = "-300px";
+    body.style.margin = "0";
+    flag = 1;
+  } else {
+    navBar.style.left = "0";
+    body.style.margin = "0 0 0 300px";
+    flag = 0;
+  }
 }
 
 hamburgerButton.addEventListener("click", toggleButton);
