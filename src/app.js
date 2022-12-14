b1c960f64837cf9548ec3946363cdfb9404e051b
Enrique Saracho Felix <esarachof97@gmail.com>
2022-12-14 10:55:03 -0700

    index.html app.js styles.css
    Updated files, added secondary navbar for shorter screen

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const hamburgerButton = document.getElementById("hamburger");
-const navBar = document.getElementById("nav-bar");
+const primaryNavBar = document.getElementById("primary-nav-bar");
 const body = document.getElementById("body");
 let flag = 0;
 
 function toggleButton() {
   if(flag == 0) {
-    navBar.style.left = "-300px";
+    primaryNavBar.style.left = "-300px";
     body.style.margin = "0";
     flag = 1;
   } else {
-    navBar.style.left = "0";
+    primaryNavBar.style.left = "0";
     body.style.margin = "0 0 0 300px";
     flag = 0;
   }
