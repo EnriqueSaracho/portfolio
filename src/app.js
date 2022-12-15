5616da265eb0a1477d1ba3cb21619db1c365db3f
Enrique Saracho Felix <esarachof97@gmail.com>
2022-12-14 20:19:46 -0700

    index.html app.js
    Updated files, secondary navbar hides after pressing links

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
+const body = document.getElementById("body");
 const primaryHamburgerButton = document.getElementById("primary-hamburger");
 const primaryNavBar = document.getElementById("primary-nav-bar");
-const secondaryHamburgerButton = document.getElementById("secondary-hamburger")
-const secondaryNavBar = document.getElementById("secondary-nav-bar")
-const body = document.getElementById("body");
 let primaryFlag = 0;
+const secondaryNavBar = document.getElementById("secondary-nav-bar");
+const secondaryHamburgerButton = document.getElementById("secondary-hamburger");
 let secondaryFlag = 0;
+const secondaryNavLogoBtn = document.getElementById("secondary-nav-logo");
+const secondaryAboutMeBtn = document.getElementById("secondary-about-me");
+const secondaryProjectsBtn = document.getElementById("secondary-projects");
+const secondaryContactMeBtn = document.getElementById("secondary-contact-me");
 
 function togglePrimaryButton() {
-  if(primaryFlag == 0) {
+  if (primaryFlag == 0) {
     primaryNavBar.style.left = "-300px";
     body.style.margin = "0";
     primaryFlag = 1;
@@ -19,7 +23,7 @@ function togglePrimaryButton() {
 }
 
 function toggleSecondaryButton() {
-  if(secondaryFlag == 0) {
+  if (secondaryFlag == 0) {
     secondaryNavBar.style.top = "0";
     body.style.marginTop = "0";
     secondaryFlag = 1;
@@ -30,5 +34,15 @@ function toggleSecondaryButton() {
   }
 }
 
+function toggleNavBarOption() {
+  secondaryNavBar.style.top = "-93vh";
+  body.style.marginTop = "7vh";
+  secondaryFlag = 0;
+}
+
 primaryHamburgerButton.addEventListener("click", togglePrimaryButton);
 secondaryHamburgerButton.addEventListener("click", toggleSecondaryButton);
+secondaryNavLogoBtn.addEventListener("click", toggleNavBarOption);
+secondaryAboutMeBtn.addEventListener("click", toggleNavBarOption);
+secondaryProjectsBtn.addEventListener("click", toggleNavBarOption);
+secondaryContactMeBtn.addEventListener("click", toggleNavBarOption);
